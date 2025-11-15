8b6917f38a0fc75700d7a0c34a092895b8d632fc
Jannatul Ferdous <137918726+Jannatul-100@users.noreply.github.com>
2025-11-15 20:48:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,137 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.itvqvzm.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Connect to the "coffeeDB" database and access its "coffee" collection
+    const coffeeCollection = client.db("coffeeDB").collection('coffee');
+
+    const userCollection = client.db("coffeeDB").collection('users');
+
+
+    app.get('/coffee', async(req, res)=>{
+        const cursor = coffeeCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/coffee/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeeCollection.findOne(query);
+        res.send(result);
+
+    })
+
+    app.post('/coffee', async (req, res)=>{
+        const newCoffee = req.body;
+        console.log(newCoffee);
+
+        const result = await coffeeCollection.insertOne(newCoffee);
+        res.send(result);
+    })
+
+    app.put('/coffee/:id', async (req, res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const options = {upsert: true};
+        const updatedCoffee = req.body;
+        const updatedCoffeeInfo ={
+            $set: {
+                name: updatedCoffee.name,
+                 chef: updatedCoffee.chef, 
+                 supplier: updatedCoffee.supplier, 
+                 taste: updatedCoffee.taste , 
+                 category: updatedCoffee.category, 
+                 details: updatedCoffee.details, 
+                 price: updatedCoffee.price, 
+                 photo: updatedCoffee.photo
+            }
+        }
+        const result = await coffeeCollection.updateOne(filter, updatedCoffeeInfo, options);
+        res.send(result);
+    })
+
+
+    app.delete('/coffee/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeeCollection.deleteOne(query);
+        res.send(result);
+    })
+
+//user api
+
+    app.get('/users', async(req, res)=>{
+        const cursor = userCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.post('/users', async(req, res) =>{
+        const newUser = req.body;
+        console.log('New User', newUser);
+        const result = await userCollection.insertOne(newUser);
+        res.send(result);
+    })
+
+    app.patch('/users', async (req, res)=>{
+        const email = req.body.email;
+        const filter = {email};
+        const updatedUser = {
+            $set: {
+                lastSignInTime: req.body?.lastSignInTime
+            }
+        }
+        const result = await userCollection.updateOne(filter, updatedUser);
+        res.send(result);
+    })
+
+    app.delete('/users/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await userCollection.deleteOne(query);
+        res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Coffee store server is running.')
+})
+
+app.listen(port, () => {
+  console.log(`Coffee store server is running on port ${port}`)
+})
